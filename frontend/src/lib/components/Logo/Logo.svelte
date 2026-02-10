80dcd262e6ffbc5d1a944c6842b2925446e7e282
muhammadmansour <58367277+muhammadmansour@users.noreply.github.com>
2026-02-10 19:28:18 +0200

    fix: rebrand emails to Wathbah GRC, fix logo in emails, add GRC text below logo in sidebar and login

--- frontend/src/lib/components/Logo/Logo.svelte
+++ frontend/src/lib/components/Logo/Logo.svelte
@@ -9,4 +9,7 @@
 	let { height = 120, width = 280 }: Props = $props();
 </script>
 
-<img class="c" {height} {width} src={logo} alt="Wathbah AI Solutions" data-testid="logo-image" />
+<div class="flex flex-col items-center">
+	<img class="c" {height} {width} src={logo} alt="Wathbah AI Solutions" data-testid="logo-image" />
+	<span class="text-lg font-semibold text-gray-700 mt-1 font-cairo">Wathbah GRC</span>
+</div>
--- frontend/src/lib/components/SideBar/SideBarHeader.svelte
+++ frontend/src/lib/components/SideBar/SideBarHeader.svelte
@@ -5,5 +5,6 @@
 <header>
 	<a href="/my-assignments" class="flex flex-col border-b pb-3 items-center justify-center">
 		<img class="w-36 h-auto" src={logo} alt="Wathbah AI Solutions" />
+		<span class="text-sm font-semibold text-gray-700 mt-1 font-cairo">Wathbah GRC</span>
 	</a>
 </header>
