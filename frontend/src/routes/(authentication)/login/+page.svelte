a1369ebab5ed352d0fd0688514cffdff76a370bf
muhammadmansour <58367277+muhammadmansour@users.noreply.github.com>
2026-02-10 19:46:24 +0200

    fix: remove greeting text from login page for cleaner layout

--- frontend/src/routes/(authentication)/login/+page.svelte
+++ frontend/src/routes/(authentication)/login/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { ActionData, PageData } from './$types';
 	import Logo from '$lib/components/Logo/Logo.svelte';
-	import Greetings from './Greetings.svelte';
 	import FormCard from './FormCard.svelte';
 
 	interface Props {
@@ -15,9 +14,6 @@
 <div class="flex flex-col items-center justify-center min-h-screen bg-slate-200 px-4">
 	<div class="flex flex-col items-center w-full max-w-md space-y-6">
 		<Logo />
-		<div class="hidden lg:block w-full">
-			<Greetings />
-		</div>
 		<div class="flex justify-center w-full">
 			<FormCard {data} {form} />
 		</div>
