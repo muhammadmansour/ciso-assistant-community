db8908895e287f4dca54823ce16c3e161622481d
muhammadmansour <58367277+muhammadmansour@users.noreply.github.com>
2026-02-11 14:58:00 +0200

    Set default language to English in PM2 config

--- start-pm2.sh
+++ start-pm2.sh
@@ -92,7 +92,7 @@ module.exports = {
         PUBLIC_BACKEND_API_URL: 'http://127.0.0.1:8000/api',
         PUBLIC_BACKEND_API_EXPOSED_URL: 'https://ciso.wathbahs.com/api',
         ORIGIN: 'https://ciso.wathbahs.com',
-        PUBLIC_DEFAULT_LANGUAGE: 'ar',
+        PUBLIC_DEFAULT_LANGUAGE: 'en',
         PORT: '3000',
         HOST: '0.0.0.0'
       },
